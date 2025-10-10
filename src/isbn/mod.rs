fb008e0b0f51556c5e1f676014e75738d41ccf11
Jonathan Otten <cubemail134@gmail.com>
2025-10-10 21:06:39 +0200

    initial commit

---
+++ src/isbn/mod.rs
@@ -0,0 +1,29 @@
+fn check_10_digit_isbn(isbn: &str) -> bool {
+    let mut sum = 0;
+    for i in 0..10 {
+        sum += (10 - i as u32) * isbn.chars().nth(i).unwrap().to_digit(10).unwrap();
+    }
+    sum % 11 == 0
+}
+
+fn check_13_digit_isbn(isbn: &str) -> bool {
+    let mut sum = 0;
+    for i in 0..12 {
+        let x = isbn.chars().nth(i).unwrap().to_digit(10).unwrap();
+        sum += (((i % 2) * 2 + 1) as u32) * x;
+    }
+    (10 - (sum % 10 as u32)) % 10 == isbn.chars().nth(12).unwrap().to_digit(10).unwrap()
+}
+
+pub fn check_isbn(isbn: &str) -> bool {
+    if !isbn.chars().all(|c| c.is_ascii_digit()) {
+        return false;
+    }
+    if isbn.len() == 10 {
+        return check_10_digit_isbn(&isbn);
+    }
+    if isbn.len() == 13 {
+        return check_13_digit_isbn(&isbn);
+    }
+    false
+}
---
+++ src/main.rs
@@ -0,0 +1,98 @@
+use chrono::{Local, NaiveDate};
+use serde::Serialize;
+use std::env;
+use std::error::Error;
+use std::fmt;
+use std::fs::File;
+use std::io::{self, Write};
+
+mod isbn;
+
+#[derive(Debug, Serialize)]
+struct Book {
+    isbn: String,
+    name: String,
+    author: String,
+    rating: u8,
+    reading_date: NaiveDate,
+    has_review: bool,
+}
+
+impl fmt::Display for Book {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "ISBN: {}\nNAME: {}\nAUTHOR: {}\nRATING: {}/10\nREADING DATE: {}",
+            self.isbn,
+            self.name,
+            self.author,
+            self.rating,
+            self.reading_date.format("%Y-%m-%d"),
+        )
+    }
+}
+
+fn ask(question: &str) -> String {
+    print!("{}", question);
+    io::stdout().flush().unwrap();
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    input.trim().to_string()
+}
+
+fn save_book(book: Book) -> Result<(), Box<dyn Error>> {
+    let file = File::create("books.csv").expect("Failed to create file books.csv");
+    let mut wtr = csv::Writer::from_writer(file);
+
+    wtr.serialize(book).expect("Failed to serialize book");
+    let _ = wtr.flush();
+    Ok(())
+}
+
+fn add_new_book() {
+    let isbn = loop {
+        let answer = ask("Please input the ISBN number of your book: ");
+        let answer = answer.replace("-", "");
+        let answer = answer.trim().to_string();
+        if isbn::check_isbn(&answer) {
+            break answer;
+        }
+        println!("wrong ISBN format");
+    };
+    let name = ask("Please input the books name: ");
+    let author = ask("Please input the authors name: ");
+    let rating = loop {
+        let answer = ask("Please rate the book from 0 to 10: ");
+        if let Ok(rating) = answer.parse::<u8>() {
+            if rating <= 10 {
+                break rating;
+            }
+        }
+        println!("Wrong format for rating");
+    };
+    let book = Book {
+        isbn: isbn,
+        name: name,
+        author: author,
+        rating: rating,
+        reading_date: Local::now().date_naive(),
+        has_review: false,
+    };
+    println!("this book is added to the file book: \n{}", book);
+    let _ = save_book(book);
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("nothing to do with so few arguments");
+        return;
+    }
+    if &args[1] == "-n" {
+        add_new_book();
+    } else {
+        println!("{} is not a recognized option", &args[1]);
+    }
+}
